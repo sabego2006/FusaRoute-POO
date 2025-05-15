1c2b402b130c97ee8175bfbf57c916e35b46c145
alejandroAvila30 <alejandro.avila3015@gmail.com>
2025-05-15 12:53:31 -0500

    creé una variable amiguitos

--- FusaRouteMain.java
+++ FusaRouteMain.java
@@ -1,2 +1,13 @@
 public class FusaRouteMain {
+
+    public static void main(String[] args) {
+        System.out.println("alfaro es gay");
+        int edad = 0;
+        boolean tuntunsahuro = false;
+
+
+    }
+
+
+
 }
