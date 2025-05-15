ebac28c5a72b96ff89b270400fddd98872b5518d
andrezzzsecso027 <alfarobrayan027@gmail.com>
2025-05-15 13:21:20 -0500

    Contructores y metodo mostrar info

--- Administrador.java
+++ Administrador.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Administrador extends Usuario{
+
+    public Administrador() {
+    }
+
+    public Administrador(String nombre, String cedula, Integer edad, String barrioResidencia, ArrayList<String> destinosFrecuentes) {
+        super(nombre, cedula, edad, barrioResidencia, destinosFrecuentes);
+    }
+    public void mostrarInfo(){
+
+    }
 
 }
