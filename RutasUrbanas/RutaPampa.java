c7b4b7e1e92923f4e3bf5c7dd8d3182455c26146
alejandroAvila30 <alejandro.avila3015@gmail.com>
2025-05-19 14:40:48 -0500

    se realizo metodo mostrar informacion

--- RutasUrbanas/RutaPampa.java
+++ RutasUrbanas/RutaPampa.java
@@ -1,4 +1,15 @@
 package RutasUrbanas;
 
-public class RutaPampa {
-}
+public class RutaPampa extends RutaUrbana {
+    public RutaPampa(){
+        super ("La Pampa","Los Girasoles ","El Gaitan","Tiempo estimado 20 min a 30 min");
+
+    }
+    @Override
+
+    public void mostrarinfoRuta(){
+        System.out.println("Ruta Urbana: La Pampa");
+        System.out.println("Paraderos: Pampa Los Girasoles, El gaitan");
+        System.out.println("tiempo estimado 20 min a 30 min ");
+        System.out.println("esta ruta hace un recorrido por: panamericana el indio, hospital, Avenida las Palmas, puente del aguila");
+        System.out.println("costo: $2.400");
